626e5173afbacc648a0d7cc396173b1d8af19146
Christian Fernau <cfernau@sonatype.com>
2018-12-10 23:11:32 +0700

    project updated

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2017-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/nexus/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+class HelloWorld {
+  String getHello() {
+    return "Hello";
+  }
+}
